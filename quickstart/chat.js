2c94b84bb78ee0d20253e8f33007885b8ac8eede
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-09-01 23:06:43 +0900

    refactor

--- quickstart/chat.js
+++ quickstart/chat.js
@@ -1,20 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 const { ChatOpenAI } = require("langchain/chat_models/openai");
-const { HumanMessage, ChatMessage, SystemMessage } = require("langchain/schema");
+const {
+  HumanMessage,
+  ChatMessage,
+  SystemMessage,
+} = require("langchain/schema");
 
 const chat = new ChatOpenAI({
-  temperature: 0
+  temperature: 0,
 });
 
-chat.predictMessages([
-  new HumanMessage("Translate this sentence from English to French. I love programming.")
-])
-.then((messages) => {
-  console.log(messages);
-});
+async function main() {
+  const result = await chat.predictMessages([
+    new HumanMessage("Say this is a test"),
+  ]);
+  console.log(result);
+}
 
 /*
   AIMessage {
     content: "J'adore la programmation."
   }
 */
+
+main();
--- quickstart/llm/llm.js
+++ quickstart/llm/llm.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-
+require("dotenv").config();
 const { OpenAI } = require("langchain/llms/openai");
 
 const llm = new OpenAI({
-  // openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0.9,
 });
 
+async function main() {
+  const result = await llm.predict(
+    "カラフルな靴下を作る会社の社名として、何がいいだろうか？"
+  );
+}
 
-
-llm.predict("カラフルな靴下を作る会社の社名として、何がいいだろうか？")
-    .then((result) => {
-        console.log(result);
-    }
-);
+main();
